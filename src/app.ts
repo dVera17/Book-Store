a5fbaf0d12b9c098b3353c16d943e6edf4cfd24d
dVera17 <dsvera174@gmail.com>
2024-11-07 11:02:38 -0500

    feat: :sparkles: Connect to database
    Connect to database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import { MongoDatabase } from "./config/db/connect"
 import { envs } from "./config/envs"
 import { AppRoutes } from "./routes/routes"
 import { Server } from "./server"
 
 const main = async () => {
+
+    await MongoDatabase.connect({
+        dbConnectionString: envs.DB_URL,
+        dbName: envs.DB_NAME
+    });
 
     new Server({
         port: envs.PORT,
